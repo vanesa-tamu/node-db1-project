5833e03d5e36dc0101e7cc942525fbd6b27053b1
vanesa-tamu <vahe5495@tamu.edu>
2020-04-24 20:27:48 -0500

    POST

--- accountsRouter.js
+++ accountsRouter.js
@@ -17,4 +17,43 @@ router.get('/:id', (req,res) => {
         })
         .catch(error => res.status(500).json({message: `error in retrieveing specific account`}))
 })
+
+router.post('/', validatePost, (req, res) => {
+    // console.log('REQ BODY', req.body)
+    const newAccount = req.body
+    db('accounts').insert(newAccount, 'id')
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(error => {
+            console.log('POST', error);
+            res.status(500).json({error: `error adding this new user!`})
+        })
+})
+
+function validatePost(req, res, next) {
+    const { name } = req.body;
+    const { budget } = req.body;
+
+    if (Object.keys(req.body).length === 0) {
+        res.status(400).json({ message: "Missing user data." });
+    } 
+    else if(!name) {
+        res.status(400).json({ message: "Missing name." });
+    }
+    else if(budget <= 0) {
+        console.log("NO BUD", budget)
+        res.status(400).json({ message: "Missing budget." });
+    }
+    else if(typeof name !== "string"){
+        return res.status(400).json({error: `must provide string for the user's name`});
+    }
+    else if(typeof budget !== "number"){
+        return res.status(400).json({error: `must provide a number for user's budget`});
+    }
+    req.body = { name, budget }
+    next();
+}
+
+
 module.exports = router;
